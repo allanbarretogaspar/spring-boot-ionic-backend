b55f3ea828687db508ec9cdaf5c4bfc75302c41b
Allan Gaspar <allanbarretogaspar@gmail.com>
2021-10-04 09:45:00 -0300

    Correções em categorias services e construtor de pagamentos

--- src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
@@ -22,54 +22,59 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 
-	public Categoria find (Integer id) {
-		
+	public Categoria find(Integer id) {
+
 		Optional<Categoria> obj = repo.findById(id);
-		
+
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
-	
+
 	public Categoria insert(Categoria obj) {
-		
+
 		obj.setId(null);
 		return repo.save(obj);
 	}
 
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
-		return repo.save(obj);
+		Categoria newObj = find(obj.getId());
+		updateDate(newObj, obj);
+		return repo.save(newObj);
 	}
 
 	public void delete(Integer id) {
 		find(id);
 		try {
-			
+
 			repo.deleteById(id);
 		} catch (DataIntegrityViolationException e) {
-			
+
 			throw new DataIntegrityException("Não é possível excluir uma categoria que contém produtos");
 		}
-		
-		
+
 	}
 
 	public List<Categoria> findAll() {
-		
+
 		return repo.findAll();
 	}
-	
-	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
-		
-		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),
-				orderBy);
-		
+
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+
 		return repo.findAll(pageRequest);
 	}
-	
+
 	public Categoria fromDTO(CategoriaDTO objDTO) {
-		
+
 		return new Categoria(objDTO.getId(), objDTO.getNome());
+	}
+
+	private void updateDate(Categoria newObj, Categoria obj) {
+
+		newObj.setNome(obj.getNome());
+
 	}
 
 }
